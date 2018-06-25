4efffb4799299d3e8d9126301eeb7197be40817d
manik dhupar <manik.dhupar7@gmail.com>
2018-06-25 14:06:37 +0530

    added total function

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {formatPrice} from '../helpers';
 
 class Order extends React.Component{
     render(){
+
+        const orderIds=Object.keys(this.props.order);
+        const total=orderIds.reduce((total,id)=>{
+            const count=this.props.order[id];
+            const fish=this.props.fishes[id];
+
+            const isAvailable= fish.status=="available";
+            if(isAvailable){
+                return total+(count*fish.price)
+            }
+            return total
+        },0)
+
+
         return(
-            <div className="order">
-            order!
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <ul>
+                    <li>{formatPrice(total)}</li>
+                </ul>
             </div>
         );
     }
